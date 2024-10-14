c8e709303accc1db4811f7a0080a595b5d725f29
dirceusljr <dirceusljr@gmail.com>
2024-10-14 15:26:06 -0300

    feat: :sparkles: feat: sort data

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -20,21 +20,29 @@ import { TasksService } from './tasks.service';
 })
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
-  // order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc';
+  order = input<'asc' | 'desc'>('desc');
+  // order?: 'asc' | 'desc';
   private tasksService = inject(TasksService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
 
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'desc') {
+          return a.id > b.id? -1 : 1;
+        }else {
+          return a.id > b.id? 1 : -1;
+        }
+      })
   );
 
   ngOnInit(): void {
-    const subscription = this.activatedRoute.queryParams.subscribe({
-      next: (params) => (this.order = params['order']),
-    });
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    // const subscription = this.activatedRoute.queryParams.subscribe({
+    //   next: (params) => (this.order = params['order']),
+    // });
+    // this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
 }
