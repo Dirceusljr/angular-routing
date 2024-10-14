a902bd420f1560beaafd37a0966ce9f4dc23a561
dirceusljr <dirceusljr@gmail.com>
2024-10-14 17:54:19 -0300

    feat: :sparkles: feat: guarding a route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  resolveTitle,
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+
+  if (shouldGetAccess < 0.5) {
+    return true;
+  } else {
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+  }
+};
 
 export const routes: Routes = [
   {
@@ -13,14 +29,12 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    canMatch: [dummyCanMatch],
     children: userRoutes,
-    // data: {
-    //   message: 'Hello!'
-    // }, para valores estáticos
     resolve: {
-      userName: resolveUserName
+      userName: resolveUserName,
     },
-    title: resolveTitle
+    title: resolveTitle,
   },
   {
     path: '**',
